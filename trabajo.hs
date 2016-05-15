12d7dba2692ed7880e2cc8dc7872d7cd40135f50
pegartillo <pegarc03@ucm.es>
2016-05-15 13:31:40 +0200

    Added all data types and started working on the functions

--- trabajo.hs
+++ trabajo.hs
@@ -3,17 +3,46 @@
 --The easiest way to think Test is a list of Questions each of them holding the number of choices and the correct one that will be expresed as a RespuestasTest
 
 --Test type
-type TestModel = Int
 type NumChoices = Int
 type CorrectChoice = Int
-data Question = Question NumChoices CorrectChoice deriving Show
-type Test = [Question]
+type ChoicesByModel = [CorrectChoice]
+data Question = Question NumChoices ChoicesByModel deriving Show
+data Test = Test [Question] deriving Show
 
 
 --RespuestasTest data type
 type ChoiceMade = Int --We are going to consider 0 as a blank answer to a question from the student
-type DNI = String
+type DNI = Int
+type TestModel = Int
 type Responses = [ChoiceMade]
-data RespuestaTest = RespuestaTest DNI TestModel Responses 
+data RespuestaTest = RespuestaTest DNI TestModel Responses deriving Show
 
-corrige::Test->RespuestaTest->Correccion
+--Correccion data type
+type CorrectAnswers = Int
+type Grade = Fractional
+data Correccion = Correccion DNI CorrectAnswers Grade deriving Show
+
+
+
+--Getters
+getQuestions::Test->[Question]
+getQuestions (Test questions) = questions
+
+getDNI::RespuestaTest->DNI
+getDNI (RespuestaTest dni _ _) = dni
+
+getResponses::RespuestaTest->Responses
+getResponses (RespuestaTest _ _ responses) = responses
+
+getModel::RespuestaTest->TestModel
+getModel (RespuestaTest _ testModel _) = testModel
+
+
+
+
+--corrige::Test->RespuestaTest->Correccion
+--corrige test responseTest = Correccion (getDNI responseTest) n (n/(length (getQuestions test)))
+--                            where n = corrigeAux (getModel responseTest) (getQuestions test) (getResponses responseTest)
+
+--corrigeAux::TestModel->[Question]->Responses->Num
+--corrigeAux _ _ _ = 0
